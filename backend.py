f4b975f33f3d36427b860078a553129353f9cbd7
jsefiu1 <jonisefiu1@gmail.com>
2023-11-04 12:55:50 +0100

    Function to get data from openweatherapi

---
+++ backend.py
@@ -0,0 +1,19 @@
+import requests
+
+API_KEY = "4d7bcc6bfe083de7c112b7df7b8936f8"
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Tirana", forecast_days=3, kind="Temperature"))
